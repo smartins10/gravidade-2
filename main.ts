ecd81f0d2d1d00e12cb8f7819cb682d5f1736ef5
smartins10 <104622338+smartins10@users.noreply.github.com>
2022-04-29 15:19:19 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
 radio.onReceivedNumber(function (receivedNumber) {
     serial.writeValue("gravidade", receivedNumber)
     led.toggle(1, 0)
-    if (receivedNumber <= 150 || receivedNumber >= 2400) {
-        music.playTone(262, music.beat(BeatFraction.Whole))
-        basic.showNumber(receivedNumber)
+    if (receivedNumber <= 150 || receivedNumber >= 2000) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.clearScreen()
     }
 })
 basic.showString("RECEPTOR GRAVIDADE")
